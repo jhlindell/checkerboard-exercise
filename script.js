718a2e1456037b12cc9355997559e2aa18c1a67f
Jon Lindell <jhlindell@yahoo.com>
2017-04-12 15:49:54 -0600

    solved random colors

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 var body = document.getElementsByTagName('body')[0];
 var checkerCounter = 1;
+
+function randomColor(){
+  let r = Math.floor(Math.random()*250);
+  let g = Math.floor(Math.random()*250);
+  let b = Math.floor(Math.random()*250);
+  console.log('rgb(' +r + ', ' + b + ', ' + g + ')');
+  return 'rgb(' +r + ', ' + b + ', ' + g + ')';
+
+}
+
 for(i=1; i<=9; i++) {
   for(j=1; j<=9; j++, checkerCounter++) {
     var element = document.createElement('div');
     element.style.width = '11.1%';
-    //element.style.height = 'auto';
     element.style.float = 'left';
-    if(checkerCounter % 2 === 0){
-      element.style.backgroundColor = 'black';
-    } else {
-      element.style.backgroundColor = 'red';
-    }
+    element.style.backgroundColor = randomColor();
     element.style.paddingBottom = '11.1%';
     body.appendChild(element);
   }
