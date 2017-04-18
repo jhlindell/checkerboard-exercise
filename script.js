6354c6a4aa9999c6b94709b2d0ed93b4706f91ca
Jon Lindell <jhlindell@yahoo.com>
2017-04-18 16:07:53 -0600

    got flashing working

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 var body = document.getElementsByTagName('body')[0];
 var checkerCounter = 1;
-for(i=1; i<=9; i++) {
-  for(j=1; j<=9; j++, checkerCounter++) {
-    var element = document.createElement('div');
-    element.style.width = '11.1%';
-    //element.style.height = 'auto';
-    element.style.float = 'left';
-    if(checkerCounter % 2 === 0){
-      element.style.backgroundColor = 'black';
-    } else {
-      element.style.backgroundColor = 'red';
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 250);
+  let g = Math.floor(Math.random() * 250);
+  let b = Math.floor(Math.random() * 250);
+  return 'rgb(' + r + ', ' + b + ', ' + g + ')';
+
+}
+
+function makeCheckers() {
+  body.innerHTML = '';
+  for (i = 1; i <= 9; i++) {
+    for (j = 1; j <= 9; j++, checkerCounter++) {
+      var element = document.createElement('div');
+      element.style.width = '11.1%';
+      element.style.float = 'left';
+      element.style.backgroundColor = randomColor();
+      element.style.paddingBottom = '11.1%';
+      body.appendChild(element);
     }
-    element.style.paddingBottom = '11.1%';
-    body.appendChild(element);
   }
-}// Your JS goes here
+}
+
+makeCheckers();
+setInterval(makeCheckers, 2000);
