737d01f798b5dab8ba141394c84c10805219414e
Jon Lindell <jhlindell@yahoo.com>
2017-04-18 15:58:28 -0600

    got gradient working

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 var body = document.getElementsByTagName('body')[0];
 var checkerCounter = 1;
-
-function randomColor(){
-  let r = Math.floor(Math.random()*250);
-  let g = Math.floor(Math.random()*250);
-  let b = Math.floor(Math.random()*250);
-  console.log('rgb(' +r + ', ' + b + ', ' + g + ')');
-  return 'rgb(' +r + ', ' + b + ', ' + g + ')';
-
-}
+var gradientOffset = 0;
 
 for(i=1; i<=9; i++) {
   for(j=1; j<=9; j++, checkerCounter++) {
     var element = document.createElement('div');
     element.style.width = '11.1%';
     element.style.float = 'left';
-    element.style.backgroundColor = randomColor();
+    if(checkerCounter % 2 === 0){
+      element.style.backgroundColor = 'rgb(0,0,0)';
+    } else {
+      element.style.backgroundColor = 'rgb(255,' + (0 + gradientOffset) + ',0)';
+    }
     element.style.paddingBottom = '11.1%';
+    //element.style.zIndex = 1;
     body.appendChild(element);
+    gradientOffset += 2;
   }
-}// Your JS goes here
+}
